c16b37d6626d39534141ea5b79a487f362a314d1
kataao <n.che@list.ru>
2022-11-02 23:10:58 +0300

    add tests with unittests usage, module3

---
+++ Module3/lesson_3_2_13_unittest.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import unittest
+
+
+class TestReg(unittest.TestCase):
+    def steps(self, link):
+        browser = webdriver.Chrome()
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, ".first_block input.first").send_keys("Ivan")
+        browser.find_element(By.CSS_SELECTOR, ".first_block input.second").send_keys("Ivanov")
+        browser.find_element(By.CSS_SELECTOR, ".first_block input.third").send_keys("Ivan.Ivanov@example.com")
+        browser.find_element(By.CSS_SELECTOR, "button.btn").click()
+        actual_welcome_text = browser.find_element(By.TAG_NAME, "h1").text
+        expected_welcome_text = "Congratulations! You have successfully registered!"
+        self.assertEqual(actual_welcome_text, expected_welcome_text,
+                         f"Actual welcome text '{actual_welcome_text}' "
+                         f"doesn't correspond to expected '{expected_welcome_text}")
+        browser.quit()
+
+    def test_reg1(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        self.steps(link)
+
+    def test_reg2(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        self.steps(link)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
